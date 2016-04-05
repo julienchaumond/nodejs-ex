344cb88f4c7121479b26cdee9cc20bd2a6d6c9fd
Julien Chaumond <julien.chaumond@orange.com>
2016-04-05 14:42:03 +0200

    Affichage variable env pour débugage

--- server.js
+++ server.js
@@ -7,18 +7,25 @@ var ParseServer = require('parse-server').ParseServer;
 
 app.engine('html', require('ejs').renderFile);
 
+console.log(process.env)
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
 var mongoURLLabel = "";
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) 
+{
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
   var mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
   var mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
   var mongoUser = process.env.MONGODB_USER
-  if (mongoHost && mongoPort && process.env.MONGODB_DATABASE) {
+
+  if (mongoHost && mongoPort && process.env.MONGODB_DATABASE) 
+  {
     mongoURLLabel = mongoURL = 'mongodb://';
-    if (process.env.MONGODB_USER && process.env.MONGODB_PASSWORD) {
+
+    if (process.env.MONGODB_USER && process.env.MONGODB_PASSWORD) 
+    {
       mongoURL += process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@';
     }
     // Provide UI label that excludes user id and pw
@@ -85,7 +92,7 @@ initDb(function(err){
 });
 
 var api = new ParseServer({
-    databaseURI: 'mongodb://'+mongoURL,
+    databaseURI: mongoURL,
     appId: 'W4APPl4ENNM4u8ht1LXy9N9g8OHQFoeM8rQpZh2B',
     masterKey: 'mNsjphrPGWCIsFMUMYGA2TlaY7bjA3isuuxs0SmR',
     push: {
